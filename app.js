983702b432904f31f7f9880bbf9b191f745d2ad8
Waseem khalil <waseemkhalilworl@gmail.com>
2024-07-22 12:14:00 -0700

    working in Typescipt

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+// let inputId = document.querySelector("#table")as HTMLFormElement
+// let input = document.querySelector("#ins") as HTMLInputElement;
+// let tableOf = document.querySelector("#time");
+// inputId.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   let tableOf = input.value;
+//   tableOf.innerHTML = "";
+//   for (let i = 1; i <= 12; i++) {
+//     const message = `${tableOf} X ${i} = ${tableOf * i}`;
+//     tableOf.innerHTML += `${message} <br>`;
+//   }
+// });
---
+++ app.ts
@@ -0,0 +1,19 @@
+
+// let inputId = document.querySelector("#table")as HTMLFormElement
+// let input = document.querySelector("#ins") as HTMLInputElement;
+// let tableOf = document.querySelector("#time");
+
+// inputId.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   let tableOf = input.value;
+
+//   tableOf.innerHTML = "";
+
+//   for (let i = 1; i <= 12; i++) {
+  
+//     const message = `${tableOf} X ${i} = ${tableOf * i}`;
+
+//     tableOf.innerHTML += `${message} <br>`;
+//   }
+// });
